b12f1b2ce0bd042b6af4dbfa617fe5a08283be97
cn99 <36594563+cn99@users.noreply.github.com>
2019-10-21 00:31:40 +0800

    Update E.java

--- src/demo/E.java
+++ src/demo/E.java
@@ -5,7 +5,7 @@ public class E {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(2222);
-		System.out.println(3333);
+		System.out.println(3333888);
 	}
 
 }
